1828ea218fba81429d81e9b53d0148e0b7f672b1
Andre Rein <ra@ra23.net>
2011-09-08 21:25:19 +0200

    Beginning to modularizing stuff.

---
+++ TPM-Prototype/src/net/ra23/tpm/config/TPMConfiguration.scala
@@ -0,0 +1,46 @@
+package net.ra23.tpm.config
+
+import scala.collection.mutable._;
+
+object TPMConfiguration {
+  var myMap = Map.empty[String, String];
+  val default =
+    <config>
+      <keyManager>
+        <tpmPassword>12345</tpmPassword>
+        <srkPassword>12345</srkPassword>
+        <keyPassword>keyPwd</keyPassword>
+        <pwdEncoding>UTF-16LE</pwdEncoding>
+      </keyManager>
+    </config>
+  def fromXML(node: scala.xml.Node) = {
+    val trimmedNode = scala.xml.Utility.trim(node);
+    trimmedNode \\ "keyManager" map {
+      keyManagerNode =>
+        {
+          keyManagerNode.child map {
+            configNode =>
+              myMap(configNode.label) = configNode.text
+          }
+        }
+    }
+  }
+  def fromXmlFile(filename: String) = {
+    fromXML(scala.xml.XML.load(filename))
+    this
+  }
+  def get(key: String) = {
+    myMap(key).toString()
+  } 
+  override def toString() = {
+    myMap.toString();
+  }
+  def toXmlFile(filename: String) {
+    println("Writing default xml to file: " + filename)
+    scala.xml.XML.saveFull(filename, default, "UTF-8", true, null);
+  }
+  //println(fromXML(atest))
+
+  //
+  //println(fromXmlFile("/tmp/config.xml"))
+}
